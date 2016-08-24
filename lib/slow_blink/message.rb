424108a4d310ba885122fef2b084ee63765f7f4b
cjhdev <cam@cjh.id.au>
2016-08-24 19:12:22 +0100

    eliminate state from Message

--- lib/slow_blink/message.rb
+++ lib/slow_blink/message.rb
@@ -2,51 +2,19 @@ module SlowBlink
 
     class Message
 
-        # Create a message instance from Blink JSON form data and a schema
-        #
-        #
         # @param schema [Schema]
         # @param data [Hash, Array<Hash>] Blink JSON Format
         # @param **opts [Hash] options
         # @raise [Error]        
-        def initialize(schema, data, **opts)
-            @schema = schema
+        def self.compact(schema, data, **opts)
             if data.kind_of? Hash
-                @data = [data]
-            else
-                @data = data
-            end                    
-        end
-
-        # Create a message instance from Blink compact form data and a schema
-        #
-        # @param schema [Schema]
-        # @param data [String] compact form encoding
-        # @return [Message]
-        # @raise [Error]
-        def self.from_compact!(schema, data)
-            # data = schema.from_compact!(data)
-            # self.new(schema, data)
-        end
-
-        # Create a Blink compact form data string from a message instance
-        #
-        # @return [String] compact form encoding
-        def to_compact
-            out = ""
-            @data.each do |i|
-                out << @schema.to_compact(i)
+                data = [data]
             end
-            out
+            data.inject("") do |out, d|
+                out << schema.to_compact(d)
+            end    
         end
-
-        # Create a Blink JSON form data structure from message instance
-        #
-        # @return [Array<Hash>] Blink JSON format
-        def to_json
-            @data
-        end
-            
+    
     end
 
 end
--- test/tc_message.rb
+++ test/tc_message.rb
@@ -27,46 +27,52 @@ class TestMessage < Test::Unit::TestCase
     def test_encode_u8
 
         schema = Schema.parse("test/0 -> u8 field")
-        m = Message.new(schema, {"$type" => "test", "field" => 42})
+        data = {"$type" => "test", "field" => 42}
+        compact = Message.compact(schema, data)
 
-        assert_equal("\x02\x00\x2a", m.to_compact)
+        assert_equal("\x02\x00\x2a", compact)
         
     end
     def test_encode_integers
 
         schema = Schema.parse("test/0 -> u8 \\u8, u16 \\u16, u32 \\u32")
-        m = Message.new(schema, {"$type" => "test", "\\u8" => 42, "\\u16" => 42, "\\u32" => 42})
+        data = {"$type" => "test", "\\u8" => 42, "\\u16" => 42, "\\u32" => 42}
+        compact = Message.compact(schema, data)
 
-        assert_equal("\x04\x00\x2a\x2a\x2a", m.to_compact)
+        assert_equal("\x04\x00\x2a\x2a\x2a", compact)
         
     end
     def test_encode_string
 
         schema = Schema.parse("test/0 -> string \\string")
-        m = Message.new(schema, {"$type" => "test", "\\string" => "hello world"})
+        data = {"$type" => "test", "\\string" => "hello world"}
+        compact = Message.compact(schema, data)
 
-        assert_equal("\x0d\x00\x0bhello world", m.to_compact)
+        assert_equal("\x0d\x00\x0bhello world", compact)
         
     end
     def test_encode_binary
 
         schema = Schema.parse("test/0 -> binary \\binary")
-        m = Message.new(schema, {"$type" => "test", "\\binary" => "hello world"})
+        data = {"$type" => "test", "\\binary" => "hello world"}
+        compact = Message.compact(schema, data)
 
-        assert_equal("\x0d\x00\x0bhello world", m.to_compact)
+        assert_equal("\x0d\x00\x0bhello world", compact)
         
     end
     def test_encode_enum
         schema = Schema.parse("colour = red | green | blue test/0 -> colour f1, colour f2, colour f3")
-        m = Message.new(schema, {"$type" => "test", "f1" => "red", "f2" => "green", "f3" => "blue"})
+        data = {"$type" => "test", "f1" => "red", "f2" => "green", "f3" => "blue"}
+        compact = Message.compact(schema, data)
 
-        assert_equal("\x04\x00\x00\x01\x02", m.to_compact)
+        assert_equal("\x04\x00\x00\x01\x02", compact)
     end
     def test_encode_sequence
         schema = Schema.parse("colour = red | green | blue test/0 -> u8 [] f1")
-        m = Message.new(schema, {"$type" => "test", "f1" => [1,2,3,4,5]})
+        data = {"$type" => "test", "f1" => [1,2,3,4,5]}
+        compact = Message.compact(schema, data)
 
-        assert_equal("\x07\x00\x05\x01\x02\x03\x04\x05", m.to_compact)
+        assert_equal("\x07\x00\x05\x01\x02\x03\x04\x05", compact)
     end
     
 end
