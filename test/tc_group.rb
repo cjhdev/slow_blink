b578287c1996313506d1d52ccf1b357412c8d1fe
cjhdev <cam@cjh.id.au>
2016-09-04 16:41:54 +0100

    bump

--- test/tc_group.rb
+++ test/tc_group.rb
@@ -24,140 +24,73 @@ class TestGroup < Test::Unit::TestCase
 
     include SlowBlink
 
-    def test_empty_group
+    def test_empty_group_without_id
         input = "Test"
         schema = Schema.new(SchemaBuffer.new(input))
-
-        assert_not_equal(nil, schema.ns[nil].group("Test"))
-
         assert_equal("Test", schema.ns[nil].group("Test").nameWithID.name)
         assert_equal(nil, schema.ns[nil].group("Test").nameWithID.id)
         assert_equal([], schema.ns[nil].group("Test").fields)
-        
+        assert_equal({}, schema.tagged)        
     end
 
     def test_empty_group_with_id
         input = "Test/0"
         schema = Schema.new(SchemaBuffer.new(input))
-
-        assert_not_equal(nil, schema.ns[nil].group("Test"))
-
         assert_equal("Test", schema.ns[nil].group("Test").nameWithID.name)
         assert_equal(0, schema.ns[nil].group("Test").nameWithID.id)
-        assert_equal([], schema.ns[nil].group("Test").fields)        
+        assert_equal([], schema.ns[nil].group("Test").fields)
+        assert_equal({0 => schema.ns[nil].group("Test")}, schema.tagged)
+    end
+
+    def test_empty_group_with_hexid
+        input = "Test/0x2a"
+        schema = Schema.new(SchemaBuffer.new(input))
+        assert_equal("Test", schema.ns[nil].group("Test").nameWithID.name)
+        assert_equal(42, schema.ns[nil].group("Test").nameWithID.id)
+        assert_equal([], schema.ns[nil].group("Test").fields)
+        assert_equal({42 => schema.ns[nil].group("Test")}, schema.tagged)
     end
 
     def test_group_of_one_field
         input = "Test -> u8 one"
         schema = Schema.new(SchemaBuffer.new(input))
-        
-        assert_not_equal(nil, schema.ns[nil].group("Test"))
-
-        assert_equal("Test", schema.ns[nil].group("Test").nameWithID.name)
-        assert_equal(nil, schema.ns[nil].group("Test").nameWithID.id)
-        assert_equal("one", schema.ns[nil].group("Test").field("one").nameWithID.name)
-        assert_equal(nil, schema.ns[nil].group("Test").field("one").nameWithID.id)
-        assert_equal(false, schema.ns[nil].group("Test").field("one").opt?)        
-    end
-
-    def test_group_of_one_field_optional
-        input = "Test -> u8 one?"
-        schema = Schema.new(SchemaBuffer.new(input))
-        assert_equal(true, schema.ns[nil].group("Test").field("one").opt?)        
-    end
-
-    def test_group_of_one_field_with_id
-        input = "Test -> u8 one/42"
-        schema = Schema.new(SchemaBuffer.new(input))
-        assert_equal(42, schema.ns[nil].group("Test").field("one").nameWithID.id)        
-    end
-
-    def test_group_of_one_field_optional_with_id
-        input = "Test -> u8 one/42?"
-        schema = Schema.new(SchemaBuffer.new(input))
-        assert_equal(true, schema.ns[nil].group("Test").field("one").opt?)        
-        assert_equal(42, schema.ns[nil].group("Test").field("one").nameWithID.id)        
+        assert_equal(1, schema.ns[nil].group("Test").fields.size)
     end
 
     def test_group_of_many_field
-        input = "Test -> u8 one, u16 two?, u32 three"
+        input = "Test -> u8 one, u8 two, u8 three"
         schema = Schema.new(SchemaBuffer.new(input))
-        
-        assert_not_equal(nil, schema.ns[nil].group("Test"))
+        assert_equal(3, schema.ns[nil].group("Test").fields.size)        
+    end
 
-        assert_equal("Test", schema.ns[nil].group("Test").nameWithID.name)
-        assert_equal(nil, schema.ns[nil].group("Test").nameWithID.id)
+    def test_with_supergroup
+        input = "Test : Super Super -> u8 one, u16 two, u32 three"        
+        schema = Schema.new(SchemaBuffer.new(input))
         assert_equal(3, schema.ns[nil].group("Test").fields.size)
-        assert_equal("one", schema.ns[nil].group("Test").field("one").nameWithID.name)
-        assert_equal("two", schema.ns[nil].group("Test").field("two").nameWithID.name)
-        assert_equal("three", schema.ns[nil].group("Test").field("three").nameWithID.name)
-        assert_equal(nil, schema.ns[nil].group("Test").field("one").nameWithID.id)
-        assert_equal(nil, schema.ns[nil].group("Test").field("two").nameWithID.id)
-        assert_equal(nil, schema.ns[nil].group("Test").field("three").nameWithID.id)
-        assert_equal(false, schema.ns[nil].group("Test").field("one").opt?)        
-        assert_equal(true, schema.ns[nil].group("Test").field("two").opt?)        
-        assert_equal(false, schema.ns[nil].group("Test").field("three").opt?)        
+        assert_true(schema.ns[nil].group("Test").group_kind_of? schema.ns[nil].group("Super"))
     end
 
-    def test_group_supergroup
-
-        input = "Test : Super Super -> u8 one, u16 two?, u32 three"        
+    def test_without_supergroup
+        input = "Test Super -> u8 one, u16 two, u32 three"        
         schema = Schema.new(SchemaBuffer.new(input))
-        assert_not_equal(nil, schema.ns[nil].group("Test"))
-
-        assert_equal("Test", schema.ns[nil].group("Test").nameWithID.name)
-        assert_equal(nil, schema.ns[nil].group("Test").nameWithID.id)
-
-        assert_equal("one", schema.ns[nil].group("Test").field("one").nameWithID.name)
-        assert_equal("two", schema.ns[nil].group("Test").field("two").nameWithID.name)
-        assert_equal("three", schema.ns[nil].group("Test").field("three").nameWithID.name)
-        assert_equal(nil, schema.ns[nil].group("Test").field("one").nameWithID.id)
-        assert_equal(nil, schema.ns[nil].group("Test").field("two").nameWithID.id)
-        assert_equal(nil, schema.ns[nil].group("Test").field("three").nameWithID.id)
-        assert_equal(false, schema.ns[nil].group("Test").field("one").opt?)        
-        assert_equal(true, schema.ns[nil].group("Test").field("two").opt?)        
-        assert_equal(false, schema.ns[nil].group("Test").field("three").opt?)        
+        assert_equal(0, schema.ns[nil].group("Test").fields.size)
+        assert_true(!schema.ns[nil].group("Test").group_kind_of?(schema.ns[nil].group("Super")))
     end
 
-    def test_group_supergroup_supergroup
+    def test_with_multiple_supergroup
 
-        input = "Test : Super Super : SuperSuper SuperSuper -> u8 one, u16 two?, u32 three"        
+        input = "Test : Super Super : SuperSuper SuperSuper -> u8 one, u16 two, u32 three"
         schema = Schema.new(SchemaBuffer.new(input))
-        assert_not_equal(nil, schema.ns[nil].group("Test"))
-
-        assert_equal("Test", schema.ns[nil].group("Test").nameWithID.name)
-        assert_equal(nil, schema.ns[nil].group("Test").nameWithID.id)
-
-        assert_equal("one", schema.ns[nil].group("Test").field("one").nameWithID.name)
-        assert_equal("two", schema.ns[nil].group("Test").field("two").nameWithID.name)
-        assert_equal("three", schema.ns[nil].group("Test").field("three").nameWithID.name)
-        assert_equal(nil, schema.ns[nil].group("Test").field("one").nameWithID.id)
-        assert_equal(nil, schema.ns[nil].group("Test").field("two").nameWithID.id)
-        assert_equal(nil, schema.ns[nil].group("Test").field("three").nameWithID.id)
-        assert_equal(false, schema.ns[nil].group("Test").field("one").opt?)        
-        assert_equal(true, schema.ns[nil].group("Test").field("two").opt?)        
-        assert_equal(false, schema.ns[nil].group("Test").field("three").opt?)        
+        assert_equal(3, schema.ns[nil].group("Test").fields.size)
+        assert_true(schema.ns[nil].group("Test").group_kind_of? schema.ns[nil].group("Super"))
+        assert_true(schema.ns[nil].group("Test").group_kind_of? schema.ns[nil].group("SuperSuper"))        
     end
 
-    def test_group_super_mix
-
-        input = "Test : Super -> u32 three Super -> u8 one, u16 two?"        
+    def test_with_mixed_supergroup
+        input = "Test : Super -> u32 three Super -> u8 one, u16 two"
         schema = Schema.new(SchemaBuffer.new(input))
-        assert_not_equal(nil, schema.ns[nil].group("Test"))
-
-        assert_equal("Test", schema.ns[nil].group("Test").nameWithID.name)
-        assert_equal(nil, schema.ns[nil].group("Test").nameWithID.id)
-
-        assert_equal("one", schema.ns[nil].group("Test").field("one").nameWithID.name)
-        assert_equal("two", schema.ns[nil].group("Test").field("two").nameWithID.name)
-        assert_equal("three", schema.ns[nil].group("Test").field("three").nameWithID.name)
-        assert_equal(nil, schema.ns[nil].group("Test").field("one").nameWithID.id)
-        assert_equal(nil, schema.ns[nil].group("Test").field("two").nameWithID.id)
-        assert_equal(nil, schema.ns[nil].group("Test").field("three").nameWithID.id)
-        assert_equal(false, schema.ns[nil].group("Test").field("one").opt?)        
-        assert_equal(true, schema.ns[nil].group("Test").field("two").opt?)        
-        assert_equal(false, schema.ns[nil].group("Test").field("three").opt?)        
-    
+        assert_equal(3, schema.ns[nil].group("Test").fields.size)
+        assert_true(schema.ns[nil].group("Test").group_kind_of? schema.ns[nil].group("Super"))
     end
 
 end
